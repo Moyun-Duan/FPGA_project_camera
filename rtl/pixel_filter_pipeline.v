7026e12d4dae9ec04675b51a093310f4aecbe9a0
Breeze-aura <auraduan666@gmail.com>
2026-06-11 10:50:48 +0800

    combine

--- rtl/pixel_filter_pipeline.v
+++ rtl/pixel_filter_pipeline.v
@@ -54,7 +54,7 @@ module pixel_filter_pipeline #(
     wire [7:0] sobel_mag;
     wire       sobel_is_edge;
 
-    sobel_pipeline u_sobel (
+    canny_pipeline u_sobel (
         .clk(clk),
         .reset(reset),
         .window_valid(win_valid),
